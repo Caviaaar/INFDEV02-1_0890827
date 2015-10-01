d5cacb17b4a32406407928ba440db5554992c3d5
Marcel Stassen <Marcel_Stassen@hotmail.com>
2015-10-01 16:07:44 +0200

    update final

--- Assignment 5/Password/Password.py
+++ Assignment 5/Password/Password.py
@@ -1,8 +1,34 @@
-password = raw_input("enter your password: ")
-
-
-# password.isalpha()
-# password.isupper()
-# password.isdigit()
-# password.isspace()
-# password.islower()
+﻿password = raw_input("enter your password: ")
+passwordpoints = 0
+if (password.isalpha() or password.isdigit() or password.isspace()):
+    print "password is unsafe and weak"
+else:
+    for i in range(len(password)):
+        char = ord(password[i])
+        if char in range(0, 48):
+            #special chars
+              passwordpoints = passwordpoints + 2
+        elif char in range(48, 58):
+            #number chars
+              passwordpoints = passwordpoints + 1
+        elif char in range (58, 65):
+            #special chars
+              passwordpoints = passwordpoints +2
+        elif char in range (65, 91):
+            #lowercase alpha
+             passwordpoints = passwordpoints + 0.5
+        elif char in range(91, 97):
+            #special chars
+             passwordpoints = passwordpoints +2
+        elif char in range (97, 123):
+            #uppercase aplha
+             passwordpoints = passwordpoints + 0.5
+        elif char in range (123 ,256):
+            #special chars
+             passwordpoints = passwordpoints + 2
+    if (passwordpoints < 5 ):
+        print ("password weak")
+    elif(passwordpoints < 10 ):
+        print ("password medium")
+    elif (passwordpoints >10 ):
+        print ("password strong")
